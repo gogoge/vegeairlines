d39415e0322a819d90920ccd7037e1aa27cf4a5f
Webb Lu <tsung.ying.lu@gmail.com>
2021-01-17 19:46:57 +0800

    Updates

---
+++ precache-manifest.586ef0b67fd08ccbe22540b8759c96b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc43d3105904695ade44e7dffe1ab6d",
+    "url": "/vegealines/index.html"
+  },
+  {
+    "revision": "e1ee90bc4986704eaefc",
+    "url": "/vegealines/static/css/main.b7ddbe6d.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/vegealines/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/vegealines/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ee90bc4986704eaefc",
+    "url": "/vegealines/static/js/main.a871e29e.chunk.js"
+  },
+  {
+    "revision": "d33735be42b7b5f57a86",
+    "url": "/vegealines/static/js/runtime-main.75914ecd.js"
+  }
+]);
